1b66e4ed74165107eb2ee01add24b4e01ff39a49
Eric Zhu <ericzhu105@gmail.com>
2021-03-09 11:06:29 -0500

    msde keyboard inputs

--- keyboard/Keyboard.js
+++ keyboard/Keyboard.js
@@ -32,3 +32,13 @@ const Keyboard = {
             });
         });
     },
+    
+    _createKeys() {
+        const fragment = document.createDocumentFragment();
+        const keyLayout = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "space"
+        ];
