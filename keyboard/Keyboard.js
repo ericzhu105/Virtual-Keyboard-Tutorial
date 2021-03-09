6ed99d0c128b4b97555cc5bef1b5f9e20035dbfc
Eric Zhu <ericzhu105@gmail.com>
2021-03-09 11:05:40 -0500

    created keyboard input options

--- keyboard/Keyboard.js
+++ keyboard/Keyboard.js
@@ -22,3 +22,13 @@ const Keyboard = {
         this.elements.keysContainer.classList.add("keyboard__keys");
         this.elements.keysContainer.appendChild(this._createKeys());
         this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    },
