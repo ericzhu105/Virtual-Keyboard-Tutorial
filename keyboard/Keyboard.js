27fe2ba7a07e251641cf843aeade7bd7f7459521
Eric Zhu <ericzhu105@gmail.com>
2021-03-09 11:02:15 -0500

    added keyboard js

---
+++ keyboard/Keyboard.js
@@ -0,0 +1,18 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
